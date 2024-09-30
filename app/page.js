ec8c90b7a954e1affc8f74a0684bf4c60d14d2c7
Meet Raut <meetraut3004@gmail.com>
2024-09-30 11:31:38 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { Homepage } from "@/components/component/homepage"
+
+export default function Home() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
+}
